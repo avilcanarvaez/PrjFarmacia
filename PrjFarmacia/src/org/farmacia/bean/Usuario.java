9fde79e24b8cf20f28787dfc61e1b11a578a09ba
ALEX <ALEX@DESKTOP-TTRUN00>
2017-07-29 20:42:00 -0500

    bean

---
+++ PrjFarmacia/src/org/farmacia/bean/Usuario.java
@@ -0,0 +1,42 @@
+package org.farmacia.bean;
+
+public class Usuario {
+	
+	private String idUsuario;
+	private String nombre;
+	private String apePaterno;
+	private String apeMaterno;
+	private String clave;
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApePaterno() {
+		return apePaterno;
+	}
+	public void setApePaterno(String apePaterno) {
+		this.apePaterno = apePaterno;
+	}
+	public String getApeMaterno() {
+		return apeMaterno;
+	}
+	public void setApeMaterno(String apeMaterno) {
+		this.apeMaterno = apeMaterno;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+
+}
